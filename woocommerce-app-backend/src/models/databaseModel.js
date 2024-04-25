da7a3ca3125aedc5bb8c817a21d307595a09470b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-04-25 15:49:20 -0300

    fix: position between elements of card (printed/date)

--- woocommerce-app-backend/src/models/databaseModel.js
+++ woocommerce-app-backend/src/models/databaseModel.js
@@ -30,7 +30,7 @@ class Database {
     date_modified
   ) {
     try {
-      // Consulta para obter o pedido no banco de dados
+      // Inserir pedido no banco de dados
       const insertOrderQuery =
         "INSERT INTO dashboard_orders(id_order, order_number, status, printed, date_created, date_modified) VALUES(?, ?, ?, ?, ?, ?)";
       const [rows] = await this.connection.query(insertOrderQuery, [
@@ -54,55 +54,54 @@ class Database {
     }
   }
 
-  async updateOrder(orderId, status, printed, date_modified) {
+  async updateOrder(orderId, status, printed, date_modified, active) {
     try {
       // Consulta para obter o pedido no banco de dados
       const updateOrderQuery =
-        "INSERT INTO dashboard_orders(id_order, status, printed, date_modified) VALUES(?, ?, ?, ?)";
-      const queryApi = [orderId, status, printed, date_modified];
-
-      const [rows] = await this.connection.query(queryDatabase, queryApi);
+        "INSERT INTO dashboard_orders(id_order, status, printed, date_modified, active) VALUES(?, ?, ?, ?) WHERE id_order = ?";
+      const [rows] = await this.connection.query(updateOrderQuery, [status, printed, date_modified, active, [orderId]]);
 
       if (!!rows && rows.length > 0) {
-        console.log("Inserção feita com sucesso");
+        console.log("Atualização do pedido feita com sucesso");
       }
+
     } catch (error) {
-      console.error("Erro ao inserir pedido no banco de dados:", error);
+      console.error("Erro ao atualizar pedido:", error);
       throw error; 
     }
   }
 
-  async deleteOrder(
-    orderId,
-    number,
-    status,
-    printed,
-    date_created,
-    date_modified
-  ) {
-    try {
-      // Consulta para obter o pedido no banco de dados
-      const queryDatabase =
-        "INSERT INTO dashboard_orders(id_order, order_number, status, printed, date_created, date_modified) VALUES(?, ?, ?, ?, ?, ?)";
-      const queryApi = [
-        orderId,
-        number,
-        status,
-        printed,
-        date_created,
-        date_modified,
-      ];
+  // async deleteOrder(
+  //   orderId,
+  //   number,
+  //   status,
+  //   printed,
+  //   date_created,
+  //   date_modified
+  // ) {
+  //   try {
+  //     // Consulta para obter o pedido no banco de dados
+  //     const queryDatabase =
+  //       "INSERT INTO dashboard_orders(id_order, order_number, status, printed, date_created, date_modified) VALUES(?, ?, ?, ?, ?, ?)";
+  //     const queryApi = [
+  //       orderId,
+  //       number,
+  //       status,
+  //       printed,
+  //       date_created,
+  //       date_modified,
+  //     ];
 
-      const [rows] = await this.connection.query(queryDatabase, queryApi);
+  //     const [rows] = await this.connection.query(queryDatabase, queryApi);
 
-      if (!!rows && rows.length > 0) {
-        console.log("Inserção feita com sucesso");
-      }
-    } catch (error) {
-      console.error("Erro ao inserir pedido no banco de dados:", error);
-      throw error; // Lança o erro para tratamento externo, se necessário
-    }
-  }
+  //     if (!!rows && rows.length > 0) {
+  //       console.log("Inserção feita com sucesso");
+  //     }
+  //   } catch (error) {
+  //     console.error("Erro ao inserir pedido no banco de dados:", error);
+  //     throw error; // Lança o erro para tratamento externo, se necessário
+  //   }
+  // }
 }
 
 module.exports = {
