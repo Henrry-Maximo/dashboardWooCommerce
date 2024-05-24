1210d14396d2f8dbab499c7a62a11ed91b0dbcc3
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-05-24 09:45:55 -0300

    config: using environment variables to connect to the database

--- woocommerce-app-backend/src/database/configDatabase.js
+++ woocommerce-app-backend/src/database/configDatabase.js
@@ -1,4 +1,7 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // const connection = mysql.createConnection({
 //     host: 'localhost',
@@ -7,18 +10,15 @@ const mysql = require('mysql2');
 //     database: 'order_analytics_db'
 // });
 
-
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'order_analytics_db',
-    waitForConnections: true,
-    connectionLimit: 100, // ou o número máximo de conexões que você deseja
-    queueLimit: 0
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 100, // ou o número máximo de conexões que você deseja
+  queueLimit: 0,
 });
-
-
 
 // pool.connect(function (err) {
 //     if (err) {
