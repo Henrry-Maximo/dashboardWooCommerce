c38154dedb051ee62074cba8254fb9b6f5df0c32
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-01-20 15:13:07 -0300

    🐛 Fix: Add certificate axios api config

--- woocommerce-app-backend/src/services/api.js
+++ woocommerce-app-backend/src/services/api.js
@@ -1,26 +1,39 @@
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-const dotenv = require("dotenv");
+const https = require("node:https");
+const fs = require("node:fs");
 
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+const dotenv = require("dotenv");
 dotenv.config();
+
+const caCert = fs.readFileSync(process.env.PATH_CRT, "utf-8");
+
+const agent = new https.Agent({
+  ca: caCert,
+  rejectUnauthorized: true, // Forçar uso do certificado
+});
 
 // instanciando WoocommerceRestApi
 const api = new WooCommerceRestApi({
   url: process.env.URL_SITE,
   consumerKey: process.env.CONSUMER_KEY,
   consumerSecret: process.env.CONSUMER_SECRET,
+  axiosConfig: {
+    httpsAgent: agent,
+  },
 });
 
 // função para buscar os pedidos da API
-async function fetchDataOrders(req, res) {
+async function fetchDataOrders(_, res) {
   try {
     // requisição para obter os dados / retornar 12 pedidos de cada status
-    const req = await api.get("logistica/dashboard?limit=12");
+    const response = await api.get("logistica/dashboard?limit=12");
 
     // retorna os dados obtidos
-    return req.data;
+    return response.data;
   } catch (error) {
-    res.status(404).end();
-    throw error; 
+    // console.error('Erro ao buscar pedidos:', error);
+    res.status(500).json({ error: 'Erro ao buscar pedidos' });
   }
 }
 
